526c4c6a4316f548626fc2471b5f86cd835ea2c6
Daniel A. Noventa <Daniel.A.Noventa@gmail.com>
2015-04-28 23:16:40 -0500

    OK, no have data showing up in a struct form.
    This seems to be getting closer to the answer. The data is in byte form.
    It requires the data to be changed from bytes to a struct. Now, to
    access all of the different data points as they come in. I feel like
    this should be a sort of ticker, with continuous incoming data. Given
    that the data is pretty simple, I just have to be able to display it in
    a proper manner. Thinking of a sort of table, but haven't found the
    right one yet. Will have to do some googling.

--- WindowsFormsApplication1/main.cs
+++ WindowsFormsApplication1/main.cs
@@ -4,6 +4,8 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -13,29 +15,73 @@ namespace WindowsFormsApplication1
 {
     public partial class main : Form
     {
-        MarketGenerator market_generator = new MarketGenerator();
+        private MarketGenerator mGen = new MarketGenerator();
+        private byte[] dBytes;
 
         public main()
         {
             InitializeComponent();
-            market_generator.Start();
         }
 
-        private void Form1_Load(object sender, EventArgs e)
+        private void Main_Load(object sender, EventArgs e)
         {
-            
+            mGen.MarketUpdate += r_MarketUpdate;
+            mGen.Start();
         }
 
-        public MarketEvent MarketUpdateEvent(object sender, MarketEvent m)
+        private void r_MarketUpdate(object sender, MarketEvent e)
         {
-            return m;
+            dBytes = e.Data;
         }
 
-        private void label1_Click(object sender, EventArgs e)
+
+        private void label1_Click(object sender, EventArgs m)
         {
-            market_generator.MarketUpdate = (MarketEvent) e;
-            label1.Text = 0.ToString();
+            //MarketEvent e = new MarketEvent();
+
+            label8.Text = dBytes.Length.ToString();
+            ByteDs ds = (ByteDs) BytesToObject(ref dBytes, typeof (ByteDs));
+            label1.Text = ds.ID.ToString();
+            label2.Text = ds.TradePrice.ToString();
+            label3.Text = ds.TradeQty.ToString();
+            label4.Text = ds.BidPrice.ToString();
+            label5.Text = ds.BidQty.ToString();
+            label6.Text = ds.AskPrice.ToString();
+            label7.Text = ds.AskQty.ToString();
+
+
         }
 
+        public static object BytesToObject(ref byte[] dataBytes, Type overlayType)
+        {
+            object result = null;
+
+            GCHandle pinnedBytes = GCHandle.Alloc(dataBytes, GCHandleType.Pinned);
+            try
+            {
+                IntPtr pinnedBytesPtr = pinnedBytes.AddrOfPinnedObject();
+
+                result = Marshal.PtrToStructure(pinnedBytesPtr, overlayType);
+            }
+            finally
+            {
+                pinnedBytes.Free();
+            }
+            return result;
+        }
+
+
+    }
+
+    [StructLayout(LayoutKind.Sequential, Size=40, Pack=1, CharSet=CharSet.Ansi)]
+    public class ByteDs
+    {
+        public int ID;
+        public double TradePrice;
+        public int TradeQty;
+        public double BidPrice;
+        public int BidQty;
+        public double AskPrice;
+        public int AskQty;
     }
 }
